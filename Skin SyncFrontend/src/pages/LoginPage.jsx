741a548fb3e4578a77f4f94bd7b182eb40f25b91
indahmutia <indahmutiatrigana@gmail.com>
2026-06-08 20:28:57 +0700

    Update LoginPage.jsx

--- Skin SyncFrontend/src/pages/LoginPage.jsx
+++ Skin SyncFrontend/src/pages/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import AuthModal from "../components/modals/AuthModal.jsx";
+import AuthModal from "../components/modals/authModal.jsx";
 import "../styles/authModal.css";
 
 export default function LoginPage() {
